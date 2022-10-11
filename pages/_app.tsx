983fe29078082d2ee0ac93d64f7004057b919136
Felipe Arosemena <farosemena@acorns.com>
2022-10-10 18:57:55 -0700

    Mount BootstrapDataProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import 'styles/globals.css'
-import type { AppProps } from 'next/app'
+import "styles/globals.css";
+import type { AppProps } from "next/app";
+import { BootstrapDataProvider } from "components/BootstrapDataProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <BootstrapDataProvider>
+      <Component {...pageProps} />
+    </BootstrapDataProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
